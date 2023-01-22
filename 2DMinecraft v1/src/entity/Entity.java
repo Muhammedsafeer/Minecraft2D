884658543d33d08c4efde7b58f896b043630dd62
Muhammedsafeer <muhammedsafeer.shefin.swafa@gmail.com>
2023-01-22 21:53:40 +0530

    Added Player Sprite

--- 2DMinecraft v1/src/entity/Entity.java
+++ 2DMinecraft v1/src/entity/Entity.java
@@ -1,8 +1,20 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 	
 	public int x, y;
 	public int speed;
+	
+	public String currentAction = "idle_left";
+	public String mainAction = "idle_left";
+
+	BufferedImage image;
+	BufferedImage[][] animations;
+	
+	int aniTick, aniIndex, aniSpeed = 30, aniCode;
+	int maxAniIndex;
+
 
 }
--- 2DMinecraft v1/src/entity/Player.java
+++ 2DMinecraft v1/src/entity/Player.java
@@ -1,7 +1,11 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -11,45 +15,117 @@ public class Player extends Entity{
 	GamePanel gp;
 	KeyHandler KeyH;
 	
+	
 	public Player(GamePanel gp, KeyHandler KeyH) {
 		
 		this.gp = gp;
 		this.KeyH = KeyH;
 		
 		setDefualtValues();
+		ImportImage();
+		loadAnimations();
 	}
 	
 	public void setDefualtValues() {
 		
 		x = 100;
 		y = 100;
-		speed = 4;
+		speed = 2;
+	}
+	
+	public void loadAnimations() {
+		animations = new BufferedImage[4][6];
+		
+		for (int j = 0; j < animations.length; j++) {
+			for (int i = 0; i < animations[j].length; i++) {
+				animations[j][i] = image.getSubimage(i * 32, j * 64, 32, 64);
+			}
+		}
+		
+	}
+	
+	public void ImportImage() {
+		
+		InputStream is = getClass().getResourceAsStream("/player/Main.png");
+		
+		try {
+			image = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				is.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void updateAnimationTick() {
+		
+		if (aniCode == 0) { maxAniIndex = 5; }
+		if (aniCode == 1) { maxAniIndex = 6; }
+		if (aniCode == 2) { maxAniIndex = 5; }
+		if (aniCode == 3) { maxAniIndex = 6; }
+		
+		aniTick++;
+		if (aniTick >= aniSpeed) {
+			aniTick = 0;
+			aniIndex++;
+			if (aniIndex >= maxAniIndex) {
+				aniIndex = 0;
+			}
+		}
+		
 	}
 	
 	public void update() {
+
+		updateAnimationTick();
+		
+		if (currentAction == "idle_left") {
+			aniCode = 0;
+		}
+		if (currentAction == "move_left") {
+			aniCode = 1;
+		}
+		if (currentAction == "idle_right") {
+			aniCode = 2;
+		}
+		if (currentAction == "move_right") {
+			aniCode = 3;
+		}
+		
 		if (KeyH.upPressed == true) {
 			
 			y -= speed;
 			
-		}else if (KeyH.downPressed == true) {
+		}
+		if (KeyH.downPressed == true) {
 			
 			y += speed;
 			
-		}else if (KeyH.leftPressed == true) {
+		}
+		if (KeyH.leftPressed == true) {
 			
+			currentAction = "move_right";
+			mainAction = "right";
 			x -= speed;
 			
-		}else if (KeyH.rightPressed == true) {
+		}if (KeyH.leftPressed == false && KeyH.rightPressed == false && mainAction == "right") { currentAction = "idle_right"; }
+		if (KeyH.rightPressed == true) {
 			
+			currentAction = "move_left";
+			mainAction = "left";
 			x += speed;
 			
-		}
+		}if (KeyH.leftPressed == false && KeyH.rightPressed == false && mainAction == "left") { currentAction = "idle_left"; }
+		
 	}
-	
+
 	public void draw(Graphics2D g2d) {
 
-		g2d.setColor(Color.white);
-		g2d.fillRect(x, y, gp.tileSize, gp.tileSize * 2);
+		g2d.drawImage(animations[aniCode][aniIndex], x, y, 64, 128, null);
 	}
 }
 
